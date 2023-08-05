ae20e9976132fc57d46e437a13e5c3a71ee2b911
Habeebunissa <habeebunissabegum26@gmail.com>
2023-08-05 21:04:04 +0400

    updated

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_complete_guide/screens/auth_screen.dart';
 
 Future<void> main() async {
